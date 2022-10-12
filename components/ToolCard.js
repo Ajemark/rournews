0056dfc16d53f1edfda968d3757a143c2f76c108
Ajemark <ajemark0110@gmail.com>
2022-10-12 14:48:49 +0100

    first commit

--- components/ToolCard.js
+++ components/ToolCard.js
@@ -1,5 +1,6 @@
 
 import Link from "@/components/Link"
+import Image from "@/components/Image"
 
 
 const ToolCard = ({ text, src, href }) => {
@@ -10,7 +11,7 @@ const ToolCard = ({ text, src, href }) => {
                     href={href}
                     className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 " >
                     <div className="flex p-4">
-                        <img alt="tool image" className="rounded-lg px-2 py-1 w-20  " src={src} />
+                        <Image width={70} height={70} alt="tool image" className="rounded-lg px-2 py-1 w-20  " src={src} />
                         <p className="text-align-center  text-lg dark:text-gray-400 px-4 w-full ">
                             {text}
                         </p>
